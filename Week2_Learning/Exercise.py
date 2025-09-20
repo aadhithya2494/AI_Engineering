a14ad1af9735db1e8942ccd5aebd3705fc471ed4
aadhithya2494 <aadhithya2494@gmail.com>
2025-09-20 11:19:11 +0530

    Create Exercise.py

---
+++ Week2_Learning/Exercise.py
@@ -0,0 +1,10 @@
+class person:
+    def __init__(my, name):
+        my.name = name
+    def invite(my):
+        print("Hi ", my.name, " you are invited to the party")
+
+p1 = person('YashAB')
+p1.invite()
+p2 = person('Kutty Pattani')
+p2.invite()
